0acebc9d402cbae442d03cedad3e33b175ea61e3
root <root@you-virtual-machine.(none)>
2011-09-14 16:05:01 +0800

    modified:   Readme 	deleted:    tcp_socket/Makefile 	deleted:    tcp_socket/Readme.txt 	deleted:    tcp_socket/client 	deleted:    tcp_socket/client.c 	deleted:    tcp_socket/output.logo

--- tcp_socket/run.sh
+++
@@ -1,4 +0,0 @@
-export PATH=$PATH:/home/forlinuxsmb/FriendlyARM/opt/FriendlyARM/toolschain/4.5.1/bin
-#arm-none-linux-gnueabi-gcc -o client client.c
-arm-none-linux-gnueabi-gcc -o Output.exe server.c
-
