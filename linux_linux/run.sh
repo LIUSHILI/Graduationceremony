bbe6ddd92df7f4c45a97425e24d0b8cc54d6ddc9
root <root@you-virtual-machine.(none)>
2011-09-14 16:10:41 +0800

    C/S linux_linux

---
+++ linux_linux/run.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/home/forlinuxsmb/FriendlyARM/opt/FriendlyARM/toolschain/4.5.1/bin
+#arm-none-linux-gnueabi-gcc -o client client.c
+arm-none-linux-gnueabi-gcc -o Output.exe server.c
+
